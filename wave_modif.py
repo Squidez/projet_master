15b4a3c1d43970a45166a7a21605fbbeaab6b446
Paul <44018884+Squidez@users.noreply.github.com>
2024-04-08 14:37:57 +0200

    Update wave_modif.py

--- wave_modif.py
+++ wave_modif.py
@@ -6,11 +6,16 @@ from torchvision.transforms import v2
 # Image test (Afrique du sud)
 img = Image.open('flags/za.png').convert('RGBA')
 img.thumbnail((1080,180))
-alpha = range(25,251, 25)
+
+
 pyplot.ion()
+
+# Liste des valeurs de la distortion elastic
+alpha = range(25,251, 25)
 
 for a in alpha:
 
+    # Applique la déformation suivant le niveau alpha
     elastic_transformer = v2.ElasticTransform(alpha=a)
     img2 = elastic_transformer(img)
 
